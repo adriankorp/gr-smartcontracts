c4e0d4262b24f8b1cbdbac4366b5dc14878ef05b
adriankorp <adrian.korpowski@gmail.com>
2023-06-24 18:36:37 +0200

    Add Token.sol

---
+++ contracts/Token.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    uint constant _initial_supply = 100000 * (10 ** 18);
+
+    constructor() ERC20("TestToken", "TT") {
+        _mint(msg.sender, _initial_supply);
+    }
+}
